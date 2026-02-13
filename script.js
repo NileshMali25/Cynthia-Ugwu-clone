c0a3b5be536d3cbd0ce7b59c1cd95382413014e2
Nilesh Supadu Mali <nilu12969@gmail.com>
2026-02-14 01:31:34 +0530

    Add mouse follower animation and circle scaling

--- script.js
+++ script.js
@@ -9,12 +9,69 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-//gsap
-    //attach gsap
+function firstPageAnim(){
+    var tl = gsap.timeline();
 
-function circleMouseFollower(){
-    window.addEventListener("mousemove",function(dets){
-        this.document.querySelector("#minicircle").style.transform=`translate($(dets.clientX)px, $(dets.clientY)px)`
+    tl.from("#nav", {
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        ease: "expo.inOut"
+    })
+
+    .to(".boundingelem", {
+        y: 0,
+        duration: 2,
+        ease: "expo.inOut",
+        delay: -1,
+        stagger: 0.2
+    })
+
+    .from("#herofooter", {   
+        y: -10,
+        opacity: 0,
+        duration: 1.5,
+        delay: -1,
+        ease: "expo.inOut"
     });
 }
+
+//jab mouse move ho to log skew kar paaye aur maximum skew and minimum skew define kr paye ,jab mouse move ho to chapta ki value badhe ,aur jab mouse chalna band ho jaye to chapta hata lo
+
+var timeout;
+
+function circleChaptaKaro(){
+    //define default scale value
+    var xscale=1;
+    var yscale=1;
+
+    var xprevious=0;
+    var yprevious=0;
+    window.addEventListener("mousemove",function(dets){
+
+        xscale=gsap.utils.clamp(.8,1.2,dets.clientX -xprevious);
+        yscale=gsap.utils.clamp(.8,1.2,dets.clientY -yprevious);
+
+        xprevious=dets.clientX;
+        yprevious=dets.clientY;
+
+        circleMouseFollower(xscale,yscale);
+        
+    });
+}
+circleChaptaKaro();
+
+
+function circleMouseFollower(xscale,yscale) {
+    window.addEventListener("mousemove", function (dets) {
+        gsap.to("#minicircle", {
+            x: dets.clientX,
+            y: dets.clientY,
+            duration: 0.3,
+            ease: "power2.out"
+        });
+    });
+}
+
 circleMouseFollower();
+firstPageAnim();
